24fd09845b4aec42b490471df565ffc895854d08
Radius <mregor@mail.ru>
2020-01-28 16:39:51 +1000

    setup

--- include/main.h
+++ include/main.h
@@ -1,5 +1,15 @@
+/* Настраиваем следующие ноги:
+PB8 - SCL I2C1 Для экрана
+PB9 - SDA I2C1
+PA6 - CH1 TIM3 Для энкодера
+PA7 - CH2 TIM3 ------
+*/
+
+
 #ifndef __MAIN_H 
 #define __MAIN_H 
-#endif
 
-void delay(uint32_t msec);
+void I2C_Init(void);
+void TIM3_Init(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -3,21 +3,34 @@
 
 int main(void)
 {
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; //Включаем порт C
-    GPIOC->CRH |= GPIO_CRH_MODE13_0;
+    TIM3_Init();
     while (1){
         GPIOC->BSRR |= GPIO_BSRR_BS13;
-        delay(100);
         GPIOC->BSRR |= GPIO_BSRR_BR13;
-        delay(100); 
     }
 	return 1;
 }
 
-void delay(uint32_t msec)
+void TIM3_Init()
 {
-    for (uint32_t j=0; j<20000UL*msec; j++)
-    {
-        __NOP();
-    }
+	// Настраиваем выводы для входа энкодера PA6 - CH1 TIM3, PA7 - CH2 TIM3
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;		         	//Включаем порт A
+	GPIOA->CRL &= ~(GPIO_CRL_CNF6_0 | GPIO_CRL_CNF7_0);	//Выводы PA6, PA7 - как вход
+	GPIOA->CRL |= GPIO_CRL_CNF6_1 | GPIO_CRL_CNF7_1;	//Выводы PA6, PA7 - pull-up
+	GPIOA->ODR |= GPIO_ODR_ODR6 | GPIO_ODR_ODR7;		//Согласно таблице на стр.161 RM0008
+
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;					// Включаем TIM3
+	TIM3->CCMR1 |= TIM_CCMR1_IC1F | TIM_CCMR1_IC2F; 	// Включаем фильтр дребезга контактов
+	TIM3->CCMR1 |= TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_0;	// Включаем канал 1 и 2 TIM3 как входы
+	TIM3->CCER &= ~(TIM_CCER_CC1P | TIM_CCER_CC2P);		// Счёт по фронту сигнала (можно не делать, это по умолчанию)
+	TIM3->SMCR |= TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1;		// Счет по фронту канала 1 и 2
+
+	TIM3->ARR = 40;		// До скольки считать
+	TIM3->CNT = 0;		// Счетчик регистра
+	TIM3->CR1 |= TIM_CR1_CEN;	//Включаем счет TIM3
+}	
+
+void I2C_Init()
+{
+
 }

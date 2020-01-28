bb0059574c0186a8bb81c7842281cffeaa6367f1
Radius <mregor@mail.ru>
2020-01-28 17:54:48 +1000

    i2c

--- src/i2c.c
+++ src/i2c.c
@@ -1,6 +1,9 @@
 #include "stm32f10x.h"
 #include "i2c.h"
 
+#define PCLK1_FREQUENCY  (SystemCoreClock / 2)
+
+
 void I2C_Init(I2C_Mode_Type mode)
 {
 	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;				// 1: I2C1 clock enabled
@@ -17,16 +20,29 @@ void I2C_Init(I2C_Mode_Type mode)
 	I2C1->CR1 |= I2C_CR1_SWRST;						// Reset I2C1
 	I2C1->CR1 = 0;									// Reset
 
-	I2C1->CR2 |= (SystemCoreClock / 1000000);		// FREQ[5:0]: Peripheral clock frequency
+/*	I2C1->CR2 |= 36;				// FREQ[5:0]: Peripheral clock frequency APB1
 
 	if (mode == I2C_STANDARD) {
-		I2C1->CCR = (SystemCoreClock / (100000 * 2));					// CCR[11:0]: Clock control register in Fm/Sm mode (Master mode) 100kHz
-		I2C1->TRISE = ((SystemCoreClock / 1000000) + 1);				// TRISE[5:0]: Maximum rise time in Fm/Sm mode (Master mode)
+		I2C1->CCR = 180;				// CCR[11:0]: Clock control register in Fm/Sm mode (Master mode) 100kHz
+		I2C1->TRISE = 37;				// TRISE[5:0]: Maximum rise time in Fm/Sm mode (Master mode)
 	} else if (mode == I2C_FAST) {
+		// Пока не работает, не считал.
 		I2C1->CCR = (SystemCoreClock / (400000 * 3));					// CCR[11:0]: Clock control register in Fm/Sm mode (Master mode) 400kHz
 		I2C1->CCR |= I2C_CCR_FS;										// 1: Fm mode I2C (Fast mode - 400kHz)
 		I2C1->TRISE = (((SystemCoreClock / 1000000 * 300) / 1000) + 1);	// TRISE[5:0]: Maximum rise time in Fm/Sm mode (Master mode)
+	}*/
+
+	I2C1->CR2 |= (PCLK1_FREQUENCY / 1000000);		// FREQ[5:0]: Peripheral clock frequency
+
+	if (mode == I2C_STANDARD) {
+		I2C1->CCR = (PCLK1_FREQUENCY / (100000 * 2));					// CCR[11:0]: Clock control register in Fm/Sm mode (Master mode) 100kHz
+		I2C1->TRISE = ((PCLK1_FREQUENCY / 1000000) + 1);				// TRISE[5:0]: Maximum rise time in Fm/Sm mode (Master mode)
+	} else if (mode == I2C_FAST) {
+		I2C1->CCR = (PCLK1_FREQUENCY / (400000 * 3));					// CCR[11:0]: Clock control register in Fm/Sm mode (Master mode) 400kHz
+		I2C1->CCR |= I2C_CCR_FS;										// 1: Fm mode I2C (Fast mode - 400kHz)
+		I2C1->TRISE = (((PCLK1_FREQUENCY / 1000000 * 300) / 1000) + 1);	// TRISE[5:0]: Maximum rise time in Fm/Sm mode (Master mode)
 	}
+
 
 	I2C1->CR1 |= I2C_CR1_PE;						// 1: Peripheral enable
 }
